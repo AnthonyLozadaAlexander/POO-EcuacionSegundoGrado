87479a4f367fe9297be44c9d6a89b47fc6fd3d0a
zDefcon <antholozadarequelme@gmail.com>
2026-04-01 22:55:55 -0500

    Agrega archivos de configuración y clases iniciales
    Se añaden archivos de configuración para el proyecto, incluyendo .gitignore, encodings.xml, misc.xml, modules.xml, y vcs.xml. También se crea la clase Raices con su estructura básica y métodos iniciales. Estos cambios establecen la base del proyecto y ayudan a gestionar el entorno de desarrollo.

---
+++ src/Clases/Raices.java
@@ -0,0 +1,34 @@
+﻿package Clases;
+
+public class Raices {
+    private double a;
+    private double b;
+    private double c;
+
+    public Raices(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    private void obtenerRaices() {
+
+    }
+
+    private void obtenerRaiz(){
+
+    }
+
+    private double getDiscriminante(){
+        return Math.pow(b, 2)-(4*a*c);
+    }
+
+    private boolean tieneRaices(){
+
+    }
+
+    private boolean tieneRaiz(){
+
+    }
+
+}
